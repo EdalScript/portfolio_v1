32bad2ec96aaff056d0eb1ccb9bcbcca7f04144a
Ed Torres <edalscript@gmail.com>
2022-03-18 20:18:08 +0300

    changes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c12f7783c41153c816b871c8f20eaf5582984b84',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c12f7783c41153c816b871c8f20eaf5582984b84',
+            'dev_requirement' => false,
+        ),
+    ),
+);
