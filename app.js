76066c936fb7df2ba26f2f8dd92f5b427a3a079e
Ed Torres <edalscript@gmail.com>
2022-03-23 17:42:32 +0300

    change to 8 seconds per rotation & replace innerHTML

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 let leading = document.getElementById('swapper');
 const text = 
 [
-"Whether it is a philosophical comment, a poor attempt at making a funny joke or a web application — You got it!" 
-, "I learned in Ruby but now I'm happily married with JavaScript"
-, 'I React a lot, but that is just my Vue'
-, 'I code for coffee — no, wait! I mean I coffee with code. Yes!'
+"\"Whether it is a philosophical comment, a poor attempt at making a funny joke or a web application — You got it!\"" 
+, "\"I learned in Ruby but now I'm happily married with JavaScript\""
+, "\"I React a lot, but that does not limit my Vue\""
+, "\"I can relocate if needed, just tell me where the coffee is\""
 ];
 let counter = 0;
-let interval = setInterval(introChange, 7000);
+let interval = setInterval(introChange, 8000);
 
 function introChange() {
-    leading.innerHTML = text[counter];
+    leading.textContent = text[counter];
     counter++;
     if (counter >= text.length) {
         counter = 0;
