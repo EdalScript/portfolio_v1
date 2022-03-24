f35b2c55a7517fdf722ad2d582459a8701429fe7
Ed Torres <edalscript@gmail.com>
2022-03-24 16:40:03 +0300

    modal

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let leading = document.getElementById('swapper');
+const leading = document.getElementById('swapper');
 const text = 
 [
 "\"Whether it is a philosophical comment, a poor attempt at making a funny joke or a web application — You got it!\"" 
@@ -7,7 +7,10 @@ const text =
 , "\"I can relocate if needed, just tell me where the coffee is\""
 ];
 let counter = 0;
-let interval = setInterval(introChange, 8000);
+const interval = setInterval(introChange, 8000);
+const modal = document.querySelector('#modal');
+const close = document.querySelector('.popup-close');
+const popup = document.querySelector('.popup-wrapper');
 
 function introChange() {
     leading.textContent = text[counter];
@@ -18,3 +21,15 @@ function introChange() {
 }
 
 introChange();
+
+modal.addEventListener('click', () => {
+    popup.style.display = 'block';
+})
+
+close.addEventListener('click', () => {
+    popup.style.display = 'none';
+})
+
+popup.addEventListener('click', () =>{
+    popup.style.display = 'none';
+})
